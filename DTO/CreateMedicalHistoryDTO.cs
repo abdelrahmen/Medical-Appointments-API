dff0eed0cc31852884bc221c2e60459460e15705
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-01 21:32:05 +0300

    DTOs for Creating and Updating Medical Histories

---
+++ DTO/CreateMedicalHistoryDTO.cs
@@ -0,0 +1,21 @@
+﻿using Medical_Appointments_API.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Medical_Appointments_API.DTO
+{
+	public class CreateMedicalHistoryDTO
+	{
+		[MaxLength(255)]
+		[Required]
+		public string MedicalCondition { get; set; }
+		[MaxLength(255)]
+		public string Medications { get; set; }
+		[MaxLength(255)]
+		public string Allergies { get; set; }
+		[MaxLength(255)]
+		public string Surgeries { get; set; }
+		[MaxLength(255)]
+		public string FamilyMedicalHistory { get; set; }
+	}
+}
---
+++ DTO/UpdateMedicalHistoryDTO.cs
@@ -0,0 +1,24 @@
+﻿using Medical_Appointments_API.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Medical_Appointments_API.DTO
+{
+    public class UpdateMedicalHistoryDTO
+    {
+        [Required]
+        public int MedicalHistoryID { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string MedicalCondition { get; set; }
+        [MaxLength(255)]
+        public string Medications { get; set; }
+        [MaxLength(255)]
+        public string Allergies { get; set; }
+        [MaxLength(255)]
+        public string Surgeries { get; set; }
+        [MaxLength(255)]
+        public string FamilyMedicalHistory { get; set; }
+    }
+}
