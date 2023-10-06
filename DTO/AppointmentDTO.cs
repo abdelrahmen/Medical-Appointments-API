81f6d4ca8cce9034d6ca6530e695491e13de7155
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-06 19:33:18 +0300

    AppointmentsDTO to return instead of Returning Appointments

---
+++ DTO/AppointmentDTO.cs
@@ -0,0 +1,39 @@
+﻿using Medical_Appointments_API.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Medical_Appointments_API.DTO
+{
+	public class AppointmentDTO
+	{
+		public int AppointmentID { get; set; }
+
+		[Required]
+		public string DoctorId { get; set; }
+
+		public string? PatientId { get; set; }
+
+		[Required]
+		public DateTime AppointmentDateTime { get; set; }
+
+		[Required]
+		[MaxLength(20)]
+		public string Status { get; set; } // e.g., Available, scheduled, canceled, completed
+
+		[MaxLength(250)]
+		public string Notes { get; set; }
+
+		public static AppointmentDTO FromAppointment(Appointment appointment)
+		{
+			return new AppointmentDTO
+			{
+				AppointmentID = appointment.AppointmentID,
+				AppointmentDateTime = appointment.AppointmentDateTime,
+				Status = appointment.Status,
+				Notes = appointment.Notes,
+				DoctorId = appointment.DoctorId,
+				PatientId = appointment.PatientId,
+			};
+		}
+	}
+}
